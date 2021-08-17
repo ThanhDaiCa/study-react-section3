d357cd35c84f515222fbbb6d103ecb17239ec729
Thanh Mai <thanhmx33@gmail.com>
2021-08-17 14:46:01 +0700

    add solution section 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const array = [
+    {title: 'Mua keo', amount: 100.10, date: new Date(2021, 8, 17)},
+    {title: 'Mua sua', amount: 10.10, date: new Date(2021, 8, 1)},
+    {title: 'Mua ca', amount: 200.10, date: new Date(2021, 8, 7)},
+    {title: 'Mua Thit', amount: 500.10, date: new Date(2021, 8, 4)}
+  ];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+     <ExpenseItem title={array[0].title} amount={array[0].amount} date={array[0].date}/>
+     <ExpenseItem title={array[1].title} amount={array[1].amount} date={array[1].date}/>
+     <ExpenseItem title={array[2].title} amount={array[2].amount} date={array[2].date}/>
+     <ExpenseItem title={array[3].title} amount={array[3].amount} date={array[3].date}/>
     </div>
   );
 }
